4db9210143e7a1380700fe6fadd9427ce1927f77
Chung-Do Kim <dorian21@naver.com>
2021-11-15 23:34:24 +0900

    chapter 4: initial commit
    cloth app

---
+++ cloth/lib/data/weather.dart
@@ -0,0 +1,30 @@
+
+
+class Weather{
+  String date;
+  int time;
+  int pop;
+  int pty;
+  String pcp;
+  int sky;
+  double wsd;
+  int tmp;
+  int reh;
+
+  Weather({this.date, this.pcp, this.pop, this.pty, this.reh, this.sky, this.time, this.tmp, this.wsd});
+
+  factory Weather.fromJson(Map<String, dynamic> data){
+
+    return Weather(
+      date: data["fcstDate"],
+      time: int.tryParse(data["fcstTime"]?? "") ?? 0,
+      pop: int.tryParse(data["POP"]?? "") ?? 0,
+      pty: int.tryParse(data["PTY"]?? "") ?? 0,
+      pcp: data["PCP"],
+      sky: int.tryParse(data["SKY"]?? "") ?? 0,
+      wsd: double.tryParse(data["WSD"]?? "") ?? 0,
+      tmp: int.tryParse(data["TMP"]?? "") ?? 0,
+      reh: int.tryParse(data["REH"]?? "") ?? 0,
+    );
+  }
+}
