3320754a9426d3529de22912718ce3739ca87c4a
Chung-Do Kim <dorian21@naver.com>
2021-12-01 14:10:57 +0900

    add calenderpage

--- dietapp/lib/main.dart
+++ dietapp/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:dietapp/view/body.dart';
 import 'package:dietapp/view/food.dart';
 import 'package:dietapp/view/workout.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import 'data/data.dart';
 import 'data/database.dart';
@@ -53,7 +54,18 @@ class _MyHomePageState extends State<MyHomePage> {
     foods = await dbHelper.queryFoodByDate(_d);
     workouts = await dbHelper.queryWorkoutByDate(_d);
     bodies = await dbHelper.queryEyeBodyByDate(_d);
-    //weight = await dbHelper.queryWeightByDate(_d);
+    weight = await dbHelper.queryWeightByDate(_d);
+
+    if(weight.isNotEmpty){
+      final w = weight.first;
+      wCtrl.text = w.weight.toString();
+      mCtrl.text = w.muscle.toString();
+      fCtrl.text = w.fat.toString();
+    }else{
+      wCtrl.text = "";
+      mCtrl.text = "";
+      fCtrl.text = "";
+    }
 
     setState((){});
   }
@@ -85,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       await Navigator.of(context).push(
                         MaterialPageRoute(builder: (ctx) => FoodAddPage(
                           food: Food(
-                            date: Utils.getFormatTime(DateTime.now()),
+                            date: Utils.getFormatTime(dateTime),
                             kcal: 0,
                             memo: "",
                             type: 0,
@@ -104,7 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       await Navigator.of(context).push(
                           MaterialPageRoute(builder: (ctx) => WorkoutAddPage(
                             workout: Workout(
-                              date: Utils.getFormatTime(DateTime.now()),
+                              date: Utils.getFormatTime(dateTime),
                               time: 60,
                               kcal: 0,
                               intense: 0,
@@ -128,7 +140,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       await Navigator.of(context).push(
                           MaterialPageRoute(builder: (ctx) => EyeBodyAddPage(
                             body: EyeBody(
-                              date: Utils.getFormatTime(DateTime.now()),
+                              date: Utils.getFormatTime(dateTime),
                               memo: "",
                               image: "",
                             ),
@@ -179,6 +191,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget getPage(){
     if(currentIndex == 0){
       return getHomeWidget(DateTime.now());
+    }else if(currentIndex == 1){
+      return getHistoryWidget();
+    }else if(currentIndex == 2){
+      return getWeightWidget();
     }
 
     return Container();
@@ -188,9 +204,12 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget getHomeWidget(DateTime date){                    //기록 탭과 홈 화면의 양식을 유사하게 만들기 위해 date를 인자로 전달하게끔함.
     return Container(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            child: foods.isEmpty ? Image.asset("assets/img/food.jpg"):ListView.builder(               //그냥 리스트뷰 빌더를 사용하게 되면 오류 발생 Container를 통해 감싸서 높이를 지정해줘야만한다
+            child: foods.isEmpty ? Container(
+              padding: EdgeInsets.all(8),
+                child: ClipRRect(child: Image.asset("assets/img/food.jpg"), borderRadius: BorderRadius.circular(12),)):ListView.builder(               //그냥 리스트뷰 빌더를 사용하게 되면 오류 발생 Container를 통해 감싸서 높이를 지정해줘야만한다
               itemBuilder: (ctx,idx) {
                 return Container(
                   height: cardSize,
@@ -205,7 +224,9 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
 
           Container(
-            child: workouts.isEmpty ? Image.asset("assets/img/workout.png") : ListView.builder(               //그냥 리스트뷰 빌더를 사용하게 되면 오류 발생 Container를 통해 감싸서 높이를 지정해줘야만한다
+            child: workouts.isEmpty ? Container(
+              padding: EdgeInsets.all(8),
+                child: ClipRRect(child: Image.asset("assets/img/workout.png"), borderRadius: BorderRadius.circular(12),)) : ListView.builder(               //그냥 리스트뷰 빌더를 사용하게 되면 오류 발생 Container를 통해 감싸서 높이를 지정해줘야만한다
               itemBuilder: (ctx,idx) {
                 return Container(
                   height: cardSize,
@@ -228,10 +249,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 }else{
                   if(bodies.isEmpty){
                     return Container(
-                      height: cardSize,
-                      width: cardSize,
-                      color: mainColor,
-                    );
+                        padding: EdgeInsets.all(8),
+                        child: ClipRRect(child: Image.asset("assets/img/body3x.png"), borderRadius: BorderRadius.circular(12),));
                   }
                   return Container(
                     height: cardSize,
@@ -255,5 +274,189 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  CalendarController calendarController = CalendarController();
+
+  Widget getHistoryWidget(){
+    return Container(
+      child: ListView.builder(
+        itemBuilder: (ctx, idx){
+          if(idx == 0){
+            return Container(
+              child: TableCalendar(
+                initialSelectedDay: dateTime,
+                calendarController: calendarController,
+                onDaySelected: (date, events, holidays){
+                  dateTime = date;
+                  getHistories();
+                },
+                headerStyle: HeaderStyle(
+                  centerHeaderTitle: true
+                ),
+                initialCalendarFormat: CalendarFormat.month,
+                availableCalendarFormats: {
+                  CalendarFormat.month:""
+                },
+              )
+            );
+          }else if (idx == 1){
+            return getHomeWidget(dateTime);
+          }
+          return Container();
+        },
+        itemCount: 2,
+      )
+    );
+  }
+
+  CalendarController weightCalendarController = CalendarController();
+  TextEditingController wCtrl = TextEditingController();               //몸무게
+  TextEditingController mCtrl = TextEditingController();                       //muscle
+  TextEditingController fCtrl = TextEditingController();                  //fat
+
+  Widget getWeightWidget(){
+    return Container(
+        child: ListView.builder(
+          itemBuilder: (ctx, idx){
+            if(idx == 0){
+              return Container(
+                  child: TableCalendar(
+                    initialSelectedDay: dateTime,
+                    calendarController: weightCalendarController,
+                    onDaySelected: (date, events, holidays){
+                      dateTime = date;
+                      getHistories();
+                    },
+                    headerStyle: HeaderStyle(
+                        centerHeaderTitle: true
+                    ),
+                    initialCalendarFormat: CalendarFormat.week,
+                    availableCalendarFormats: {
+                      CalendarFormat.week:""
+                    },
+                  )
+              );
+            }else if (idx == 1){
+              return Container(
+                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("${dateTime.month}월 ${dateTime.day}일"),
+                        InkWell(
+                          child: Container(
+                            child: Text("저장"),
+                            decoration: BoxDecoration(
+                              color: mainColor,
+                              borderRadius: BorderRadius.circular(8)
+                            ),
+                            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                          ),
+                          onTap: () async {
+                            Weight w;
+                            if(weight.isEmpty){
+                              //몸무게를 처음 입력하는 것이다
+                              w = Weight(date: Utils.getFormatTime(dateTime));
+                            }else{
+                              //처음입력이아니다
+                              w = weight.first;
+                            }
+
+                            w.weight = int.tryParse(wCtrl.text) ?? 0;
+                            w.muscle = int.tryParse(mCtrl.text) ?? 0;
+                            w.fat = int.tryParse(fCtrl.text) ?? 0;
+
+                            await dbHelper.insertWeight(w);
+                            //getHistories();
+                          },
+                        )
+                      ],
+                    ),
+
+                    Container(height: 12),
+
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Container(width: 8),
+                              Text("몸무게"),
+                              TextField(
+                                  keyboardType: TextInputType.number,              //숫자만 입력 가능하게 만든다
+                                  controller: wCtrl,
+                                  textAlign: TextAlign.end,
+                                  decoration: InputDecoration(
+                                      border: UnderlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: txtColor,
+                                              width: 0.5
+                                          )
+                                      )
+                                  )
+                              )
+                            ],
+                          )
+                        ),
+                        Container(width: 8),
+                        Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Text("근육량"),
+                                TextField(
+                                    keyboardType: TextInputType.number,              //숫자만 입력 가능하게 만든다
+                                    controller: mCtrl,
+                                    textAlign: TextAlign.end,
+                                    decoration: InputDecoration(
+                                        border: UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: txtColor,
+                                                width: 0.5
+                                            )
+                                        )
+                                    )
+                                )
+                              ],
+                            )
+                        ),
+                        Container(width: 8),
+                        Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Text("지방"),
+                                TextField(
+                                    keyboardType: TextInputType.number,              //숫자만 입력 가능하게 만든다
+                                    controller: fCtrl,
+                                    textAlign: TextAlign.end,
+                                    decoration: InputDecoration(
+                                        border: UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: txtColor,
+                                                width: 0.5
+                                            ),
+                                          borderRadius: BorderRadius.circular(8)
+                                        )
+                                    ),
+                                )
+                              ],
+                            ),
+                        ),
+                        Container(width: 8),
+                      ]
+                    )
+                  ],
+                ),
+              );
+            }
+            return Container();
+          },
+          itemCount: 2,
+        )
+    );
+  }
 
 }
